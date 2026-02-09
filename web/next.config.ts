8552c6f3f3408e1884eb0beb8c171867274db4e1
Moana <moanavaai09@gmail.com>
2026-02-09 22:12:59 +1300

    Allow external directory imports in Next.js

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This tells Next.js to allow and compile imports from the parent directory
+  experimental: {
+    externalDir: true,
+  },
+  // If there are specific packages we need to transpile, we can add them here
+  transpilePackages: ["@core"],
 };
 
 export default nextConfig;
